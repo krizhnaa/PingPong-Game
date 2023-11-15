52db757a164e3860a53f2cc3e7dee59393f3d5c3
krizhnaa <krizhnaprem@gmail.com>
2023-11-15 21:08:31 +0530

    Created the ball

---
+++ ball.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color('white')
+        self.shape('circle')
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        # self.goto(x, y)
